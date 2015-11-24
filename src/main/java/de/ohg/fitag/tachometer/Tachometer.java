2c77d0417704e4c5c031614be8699d254a3db8a5
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2015-11-24 21:31:16 +0100

    initital

---
+++ src/main/java/de/ohg/fitag/tachometer/Tachometer.java
@@ -0,0 +1,15 @@
+package de.ohg.fitag.tachometer;
+
+
+import lejos.pc.comm.NXTComm;
+
+/**
+ * Created by Calvin on 24.11.2015.
+ */
+public class Tachometer {
+
+    public static void main(String[] args){
+        System.out.println("Hello world! Built using gradle");
+    }
+
+}
